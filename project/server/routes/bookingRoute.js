833a5d8f790422dc0822dde5970ad1c6c7469ffc
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2024-06-28 13:53:36 +0530

    Update bookingRoute.js

--- project/server/routes/bookingRoute.js
+++ project/server/routes/bookingRoute.js
@@ -1,106 +1,124 @@
-const router = require('express').Router();
-const stripe = require('stripe')('sk_test_51JKPQWSJULHQ0FL7LbqLKOaIcjurlUcdP2hJQkXZw3txlhh0hFrEEEOTwdVxf6sWKqLIrerKpV5EfGvmvntYu7Mt00vJq4YQKL');
+const router = require("express").Router();
+const stripe = require("stripe")(
+  "sk_test_51JKPQWSJULHQ0FL7LbqLKOaIcjurlUcdP2hJQkXZw3txlhh0hFrEEEOTwdVxf6sWKqLIrerKpV5EfGvmvntYu7Mt00vJq4YQKL"
+);
 const authMiddleware = require("../middlewares/authMiddleware");
-const Booking = require('../models/bookingModel');
-const Show = require('../models/showModel');
+const Booking = require("../models/bookingModel");
+const Show = require("../models/showModel");
+const EmailHelper = require("../utils/emailSender");
 
-router.post('/make-payment',  async (req, res) => {
-    try{
-        const {token, amount} = req.body;
-        const customer = await stripe.customers.create({
-            email: token.email,
-            source: token.id
-        });
+router.post("/make-payment", async (req, res) => {
+  try {
+    const { token, amount } = req.body;
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    });
 
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: amount,
-            currency: 'usd',
-            customer: customer.id,
-            payment_method_types: ['card'],
-            receipt_email: token.email,
-            description: "Token has been assigned to the movie!"
-        });
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: "usd",
+      customer: customer.id,
+      payment_method_types: ["card"],
+      receipt_email: token.email,
+      description: "Token has been assigned to the movie!",
+    });
 
-        // const charge = await stripe.charges.create({
-        //     amount: amount,
-        //     currency: "usd",
-        //     customer: customer.id,
-        //     receipt_email: token.email,
-        //     description: "Token has been assigned to the movie!"
-        // });
-        
-        const transactionId = paymentIntent.id;
+    // const charge = await stripe.charges.create({
+    //     amount: amount,
+    //     currency: "usd",
+    //     customer: customer.id,
+    //     receipt_email: token.email,
+    //     description: "Token has been assigned to the movie!"
+    // });
 
-        res.send({
-            success: true,
-            message: "Payment Successful! Ticket(s) booked!",
-            data: transactionId
-        });
-    }catch(err){
-        res.send({
-            success: false,
-            message: err.message
-        })
-    }
+    const transactionId = paymentIntent.id;
+
+    res.send({
+      success: true,
+      message: "Payment Successful! Ticket(s) booked!",
+      data: transactionId,
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 // Create a booking after the payment
-router.post('/book-show',  async (req, res) => {
-    try{
-        const newBooking = new Booking(req.body);
-        await newBooking.save();
+router.post("/book-show", async (req, res) => {
+  try {
+    const newBooking = new Booking(req.body);
+    await newBooking.save();
 
-        const show = await Show.findById(req.body.show).populate("movie");
-        const updatedBookedSeats = [...show.bookedSeats, ...req.body.seats];
-        await Show.findByIdAndUpdate(req.body.show, { bookedSeats: updatedBookedSeats });
-        res.send({
-            success: true,
-            message: 'New Booking done!',
-            data: newBooking
-        });
-    }catch(err){
-        res.send({
-            success: false,
-            message: err.message
-        });
-    }
+    const show = await Show.findById(req.body.show).populate("movie");
+    const updatedBookedSeats = [...show.bookedSeats, ...req.body.seats];
+    await Show.findByIdAndUpdate(req.body.show, {
+      bookedSeats: updatedBookedSeats,
+    });
+
+    const populatedBooking = await Booking.findById(newBooking._id).populate({
+      path: "user",
+      select: "email name", // Specify the fields you want to include
+    });
+
+    console.log("this is populated Booking", populatedBooking);
+    console.log(populatedBooking.user.email);
+
+    res.send({
+      success: true,
+      message: "New Booking done!",
+      data: populatedBooking,
+    });
+
+    await EmailHelper("ticketTemplate.html", populatedBooking.user.email, {
+       name: populatedBooking.user.name,
+       seats : populatedBooking.seats,
+       amount : populatedBooking.seats.length * 200,
+       transactionId : populatedBooking.transactionId
+
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
+router.get("/get-all-bookings", authMiddleware, async (req, res) => {
+  try {
+    const bookings = await Booking.find({ user: req.body.userId })
+      .populate("user")
+      .populate("show")
+      .populate({
+        path: "show",
+        populate: {
+          path: "movie",
+          model: "movies",
+        },
+      })
+      .populate({
+        path: "show",
+        populate: {
+          path: "theatre",
+          model: "theatres",
+        },
+      });
 
-router.get("/get-all-bookings",authMiddleware, async (req, res) => {
-    try{
-        const bookings = await Booking.find({ user: req.body.userId })
-        .populate("user")
-        .populate("show")
-            .populate({
-                path: "show",
-                populate: {
-                    path: "movie",
-                    model: "movies"
-                }
-            })
-            .populate({
-                path: "show",
-                populate: {
-                    path: "theatre",
-                    model: "theatres"
-                }
-            });
-        
-        res.send({
-            success: true,
-            message: "Bookings fetched!",
-            data: bookings
-        })
-
-    }catch(err){
-        res.send({
-            success: false,
-            message: err.message
-        })
-    }
+    res.send({
+      success: true,
+      message: "Bookings fetched!",
+      data: bookings,
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: err.message,
+    });
+  }
 });
-
-
 
 module.exports = router;
